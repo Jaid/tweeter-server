936aa1e3fd225c8d00049455dfd36c0e30afd5b6
Jaid <jaid.jsx@gmail.com>
2019-07-14 10:35:09 +0200

    improve: Improved error handling
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/api.js
+++ src/api.js
@@ -32,39 +32,43 @@ class Api {
 
   async init() {
     app.post("/tweet", bodyParser.json(), async (request, response) => {
-      logger.debug("Got post data with keys %s", Object.keys(request.body).join())
-      for (const requiredArgument of ["text", "handle", "apiUser", "apiKey"]) {
-        if (!request.body?.[requiredArgument]) {
-          response.send(`body.${requiredArgument} not given`)
-          return
-        }
-      }
-      const apiUser = this.getApiUser(request.body.apiUser, request.body.apiKey)
-      if (!apiUser) {
-        response.send("Invalid API User")
-        logger.warn("Someone tried to send a tweet with user %s and key %s", request.body.apiUser, request.body.apiKey)
-        return
-      }
-      const handle = request.body.handle.toLowerCase()
-      if (request.body.media) {
-        for (const dataUrl of ensureArray(request.body.media)) {
-          const {body, mimeType} = dataUrls(dataUrl)
-          const size = body.length
-          logger.info(`Got media of type ${mimeType.essence}`)
-          if (mimeType.type === "image") {
-            const mediaId = shortid()
-            const mediaFolder = path.join(appFolder, "cache", handle, mediaId)
-            await fsp.outputFile(path.join(mediaFolder, "image.png"), body)
-            logger.info("Saved media %s, %s bytes", mediaId, size)
+      try {
+        logger.debug("Got post data with keys %s", Object.keys(request.body).join())
+        for (const requiredArgument of ["text", "handle", "apiUser", "apiKey"]) {
+          if (!request.body?.[requiredArgument]) {
+            response.send(`body.${requiredArgument} not given`)
+            return
           }
         }
+        const apiUser = this.getApiUser(request.body.apiUser, request.body.apiKey)
+        if (!apiUser) {
+          response.send("Invalid API User")
+          logger.warn("Someone tried to send a tweet with user %s and key %s", request.body.apiUser, request.body.apiKey)
+          return
+        }
+        const handle = request.body.handle.toLowerCase()
+        if (request.body.media) {
+          for (const dataUrl of ensureArray(request.body.media)) {
+            const {body, mimeType} = dataUrls(dataUrl)
+            const size = body.length
+            logger.info(`Got media of type ${mimeType.essence}, ${mimeType.type}`)
+            if (mimeType.type === "image") {
+              const mediaId = shortid()
+              const mediaFolder = path.join(appFolder, "cache", handle, mediaId)
+              await fsp.outputFile(path.join(mediaFolder, "image.png"), body)
+              logger.info("Saved media %s, %s bytes", mediaId, size)
+            }
+          }
+        }
+        logger.info("[User %s] @%s: %s", request.body.apiUser, handle, request.body.text)
+        const tweetResult = await twitterClient.tweet(handle, request.body.text)
+        if (tweetResult.statusCode !== 200) {
+          logger.warn("Sending tweet may not have worked. Got %s %s", tweetResult.statusCode, tweetResult.statusMessage)
+        }
+        response.send(tweetResult.statusMessage)
+      } catch (error) {
+        logger.error("Could not handle /tweet request: %s", error)
       }
-      logger.info("[User %s] @%s: %s", request.body.apiUser, handle, request.body.text)
-      const tweetResult = await twitterClient.tweet(handle, request.body.text)
-      if (tweetResult.statusCode !== 200) {
-        logger.warn("Sending tweet may not have worked. Got %s %s", tweetResult.statusCode, tweetResult.statusMessage)
-      }
-      response.send(tweetResult.statusMessage)
     })
     logger.info("Started API server")
   }

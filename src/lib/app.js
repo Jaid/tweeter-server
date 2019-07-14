1348d6dd46d8af80259d2a61ae152baeb22d832f
Jaid <jaid.jsx@gmail.com>
2019-07-14 09:12:39 +0200

    improve: Added proper `express` request logging
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/lib/app.js
+++ src/lib/app.js
@@ -6,6 +6,7 @@ import logger from "lib/logger"
 import express from "express"
 
 const app = express()
+
 http.createServer(app).listen(config.apiPort)
 logger.info("Started HTTP server on port %s", config.apiPort)
 
@@ -13,5 +14,11 @@ if (config.apiSslPort) {
   https.createServer(app).listen(config.apiSslPort)
   logger.info("Started HTTPS server on port %s", config.apiSslPort)
 }
+
+app.use(function (request, response, next) {
+  const ip = request.headers["x-forwarded-for"] || request.connection.remoteAddress
+  logger.debug("[%s] -> %s %s", ip, request.method, request.originalUrl)
+  next()
+})
 
 export default app

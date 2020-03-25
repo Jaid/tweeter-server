d5bbf6b4ed68e2545d2223ad4e8de1e1f29d0f19
Jaid <jaid.jsx@gmail.com>
2020-03-25 09:51:30 +0100

    Removed test
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -1,9 +1,1 @@
-import delay from "delay"
-import ms from "ms.macro"
-import path from "path"
-
-it("should run", async () => {
-  (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
-  await delay(ms`30 seconds`)
-  process.exit(0)
-}, ms`40 seconds`)
+test.todo("Here could be some tests maybe!")
